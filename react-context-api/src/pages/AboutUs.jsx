50f1a38774be57d5ec6239e7948460642eaf9ab8
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-05-19 15:23:56 +0200

    [UPD] Created About/Contact/HomePage

---
+++ react-context-api/src/pages/AboutUs.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const AboutUs = () => {
+    return (
+        <>
+            <NavBar />
+            <div className="hp-cont">
+                <h1>Chi siamo</h1>
+                <p>Siamo semplici e umili venditori. I nostri artigiani sono dei puri maestri del creato. Ogni giorno creano eccellenti prodotti di inestimabile qualità e valore. Chiaramente per voi alla modica cifra di 20€.</p>
+            </div>
+        </>
+    )
+}
+
+export default AboutUs
