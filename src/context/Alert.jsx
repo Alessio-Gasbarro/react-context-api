bae4b1ac216fcd829152432fed7abd5d680bc15b
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-05-20 10:35:35 +0200

    [UPD] Added Alert

---
+++ src/context/Alert.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useAlertContext } from './AlertContext'
+
+const Alert = () => {
+    const { alertData } = useAlertContext();
+    return (
+        <div className={`alert alert-${alertData.type}`}>
+            {alertData.message}
+        </div>
+    )
+}
+
+export default Alert;
